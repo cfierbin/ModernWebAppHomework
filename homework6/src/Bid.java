2f3e52ce4653db23d262301ba26c151a0add23ec
Cristina Fierbinteanu <cristina.fierbinteanu@gmail.com>
2014-09-08 10:54:49 +0300

    started to work on homework6

---
+++ homework6/src/Bid.java
@@ -0,0 +1,15 @@
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+/**
+ * Created by yfain11 on 4/4/14.
+ */
+public class Bid {
+    public int id;
+    public Product product;
+    public BigDecimal amount;
+    public int desiredQuantity; // How many items the user wants
+    public User user;
+    public LocalDateTime bidTime;
+    public boolean isWinning;
+}
---
+++ homework6/src/User.java
@@ -0,0 +1,9 @@
+/**
+ * Created by yfain11 on 4/4/14.
+ */
+public class User {
+    public int id;
+    public String name;
+    public String email;
+    public boolean getOverbidNotifications;
+}
